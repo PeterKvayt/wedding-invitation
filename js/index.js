28cd874969fdf6c5306480240f0508d5183a1b8f
PeterKvayt <peter.kvayt@gmail.com>
2026-03-18 20:23:31 +0300

    changes

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const _weddingDateMoment = new Date(2026, 4, 10, 17, 0, 0).getTime();
+const _weddingDateMoment = new Date(2026, 4, 10, 16, 0, 0).getTime();
 const _secondInMillisecond = 1000;
 const _minuteInMillisecond = _secondInMillisecond * 60;
 const _hourInMillisecond = _minuteInMillisecond * 60;
@@ -9,7 +9,6 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 function main() {
-    // ymaps.ready(initMapV2);
     setupCountDown();
 }
 
@@ -23,46 +22,38 @@ function updateCountDown(){
     const diffInMilliSeconds = Math.max(_weddingDateMoment - nowMoment, 0);
     
     const days = Math.floor(diffInMilliSeconds / _dayInMillisecond);
+    document.getElementById("count-down-days--text").innerText = transformText(days, "День", "Дня", "Дней");
     document.getElementById("count-down-days").innerText = days;
     
     const hours = Math.floor((diffInMilliSeconds % _dayInMillisecond) / _hourInMillisecond);
+    document.getElementById("count-down-hours--text").innerText = transformText(hours, "Час", "Часа", "Часов");
     document.getElementById("count-down-hours").innerText = hours;
     
     const minutes = Math.floor((diffInMilliSeconds % _hourInMillisecond) / _minuteInMillisecond);
+    document.getElementById("count-down-minutes--text").innerText = transformText(minutes, "Минута", "Минуты", "Минут");
     document.getElementById("count-down-minutes").innerText = minutes;
     
     const seconds = Math.floor((diffInMilliSeconds % _minuteInMillisecond) / _secondInMillisecond);
+    document.getElementById("count-down-seconds--text").innerText = transformText(seconds, "Секунда", "Секунды", "Секунд");
     document.getElementById("count-down-seconds").innerText = seconds;
 }
 
-async function initMapV3() {
-    await ymaps3.ready;
+function transformText(value, text1, text2, text3) {
+    if (value > 10 && value < 19) {
+        return text3;
+    }
+    
+    const stringValue = value.toString();
 
-    const { YMap, YMapDefaultSchemeLayer } = ymaps3;
+    if (stringValue.endsWith("1")) {
+       return text1; 
+    }
 
-    const map = new YMap(
-        document.getElementById('map'),
-        {
-            location: {
-                center: [37.588144, 55.733842],
-                zoom: 10
-            }
-        }
-    );
+    if (stringValue.endsWith("2") 
+        || stringValue.endsWith("3")
+        || stringValue.endsWith("4")) {
+       return text2; 
+    }
 
-    // Добавляем слой для отображения схематической карты
-    map.addChild(new YMapDefaultSchemeLayer());
-}
-
-function initMapV2() {
-    var myMap = new ymaps.Map("map", {
-            // Координаты центра карты.
-            // Порядок по умолчанию: «широта, долгота».
-            // Чтобы не определять координаты центра карты вручную,
-            // воспользуйтесь инструментом Определение координат.
-            center: [55.76, 37.64],
-            // Уровень масштабирования. Допустимые значения:
-            // от 0 (весь мир) до 19.
-            zoom: 7
-        });
+    return text3;
 }
